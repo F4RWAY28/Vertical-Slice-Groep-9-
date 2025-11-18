b6100efb0d91a2d5b4d2e5ff0abc9de5ddb7d80c
D'onell/F4RWAY <donfrank2007@gmail.com>
2025-11-18 12:43:52 +0100

    'ScriptAddedButVisualStudioIsActingWeird'

---
+++ Assets/Scripts/Player/Movement/Movement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float walkingSpeed = 2f;
+    private float runningSpeed = 8f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(Keycode.W))
+        {
+            
+        }
+    }
+}
